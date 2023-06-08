63d07d580726333dee53aae5e07b9aca6c19b4ee
Mitchell Thomas <mt@mt.codes>
2023-06-08 15:00:25 -0400

    fix up dashboard slightly

--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,6 @@
 <template>
     <v-container>
-      <v-row justify="center" align="center">
-        <v-col style="text-align: center;" cols="12">
-          <h2>Patient Portal / Notes Console</h2>
-          <v-card class="my-5 pa-2">
-            <v-card-title>Items to test</v-card-title>
-            <div style="text-align: left;">
-              <v-list>
-                <v-list-item>Add/Update Patient</v-list-item>
-                <v-list-item>Add/Update Note</v-list-item>
-                <v-list-item>Add/Update Note</v-list-item>
-                <v-list-item>Add/Update Spinal Entries for Note</v-list-item>
-                <v-list-item>Add/Update Extremity Entries for Note</v-list-item>
-                <v-list-item>Once A Few entries are added per note, Export Note to CSV, Excel</v-list-item>
-              </v-list>
-            </div>
-          </v-card>
-        </v-col>
-
-        <!-- <v-col cols="6" sm="8" md="6">
-          <v-hover
-          v-slot="{ isHovering, props }"
-          open-delay="50"
-        >
-          <v-card
-            :elevation="isHovering ? 16 : 2"
-            :class="{ 'on-hover': isHovering }"
-            class="mx-auto"
-            height="200"
-            v-bind="props"
-          >
-            <v-card-text class="font-weight-medium mt-12 text-center text-subtitle-1">
-              Open Delay (Mouse enter)
-            </v-card-text>
-          </v-card>
-        </v-hover>
-        </v-col>
-        <v-col cols="6" sm="8" md="6">
-          <v-hover
-          v-slot="{ isHovering, props }"
-          open-delay="50"
-        >
-          <v-card
-            :elevation="isHovering ? 16 : 2"
-            :class="{ 'on-hover': isHovering }"
-            class="mx-auto"
-            height="200"
-            v-bind="props"
-          >
-            <v-card-text class="font-weight-medium mt-12 text-center text-subtitle-1">
-              Open Delay (Mouse enter)
-            </v-card-text>
-          </v-card>
-        </v-hover>
-        </v-col> -->
-      </v-row>
+      
 
       <!-- chart section  -->
       <v-row>
