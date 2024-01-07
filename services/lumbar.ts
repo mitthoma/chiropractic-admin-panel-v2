f8617dc7d37940cc22fa09b77951bc6053923d3e
Mitchell Thomas <mt@macbook-air.mynetworksettings.com>
2024-01-07 17:41:17 -0500

    frontend services are built

---
+++ services/lumbar.ts
@@ -0,0 +1,28 @@
+import { AxiosInstance } from 'axios';
+
+export const createLumbarService = (api: AxiosInstance) => ({
+  getLumbarsForReport: async (payload: any) => {
+    const { data } = await api.post('/lumbar/report-lumbars', payload);
+    return data.data;
+  },
+
+  getLumbar: async (payload: any) => {
+    const { data } = await api.get('/lumbar/lumbar', payload);
+    return data.data;
+  },
+
+  addLumbar: async (payload: any) => {
+    const { data } = await api.post('/lumbar/lumbar', payload);
+    return data.data;
+  },
+
+  updateLumbar: async (payload: any) => {
+    const { data } = await api.post('/lumbar/update', payload);
+    return data.data;
+  },
+
+  deleteLumbar: async (payload: any) => {
+    const { data } = await api.post('/lumbar/delete', payload);
+    return data.data;
+  },
+});
