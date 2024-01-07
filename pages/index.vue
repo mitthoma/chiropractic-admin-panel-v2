8c6fe8e9517bbc37507fa2dce6b2143ecf9256ad
Mitchell Thomas <mt@macbook-air.mynetworksettings.com>
2024-01-07 16:47:34 -0500

    modify the dashboard view / init and location of patient report component

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,79 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12">
-        <v-card>
-          <v-card-title
-            >Welcome Pynkerton Chiropractic Notes Dashboard</v-card-title
-          >
+      <v-col cols="3"></v-col>
+
+      <v-col cols="6">
+        <v-card class="text-center">
+          <v-card-title>Notes Dashboard</v-card-title>
         </v-card>
       </v-col>
+      <v-col cols="3"></v-col>
+
+      <v-col cols="3"></v-col>
+      <v-col cols="6">
+        <v-card>
+          <v-card-text class="text-center">
+            <h3 class="my-5">Please select a patient to get started</h3>
+            <v-select
+              v-model="selectedPatient"
+              :items="fetchedPatients"
+              :item-title="patientDisplayName"
+              item-value="id"
+              label="Select a patient"
+            ></v-select>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="3"></v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { createPatientService } from '~~/services/patient';
+
 export default {
   name: 'DashboardPage',
   data() {
     return {
-      chartData: {
-        labels: [
-          'Jan',
-          'Feb',
-          'Mar',
-          'Apr',
-          'May',
-          'Jun',
-          'Jul',
-          'Aug',
-          'Sep',
-          'Oct',
-          'Nov',
-          'Dec',
-        ],
-        datasets: [
-          {
-            label: 'Example Data',
-            data: [12, 19, 3, 5, 2, 3, 10, 15, 8, 23, 12, 7],
-            borderColor: 'rgba(75, 192, 192, 1)',
-            tension: 0.1,
-          },
-        ],
-      },
-      chartOptions: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
+      patients: [],
+      selectedPatient: null,
+      patientService: null,
     };
+  },
+  computed: {
+    fetchedPatients() {
+      return this.patients.map((patient) => ({
+        ...patient,
+        displayName: this.patientDisplayName(patient),
+      }));
+    },
+  },
+  watch: {
+    selectedPatient(newValue) {
+      if (newValue) {
+        this.$router.push(`/patient/${newValue}`);
+      }
+    },
+  },
+  async mounted() {
+    try {
+      this.patientService = createPatientService(this.$api);
+      this.patients = await this.patientService.getPatients();
+    } catch (error) {
+      console.error('Error fetching patients:', error);
+      // Handle error appropriately
+    }
+  },
+  methods: {
+    patientDisplayName(patient) {
+      if (patient && patient.firstName && patient.lastName && patient.acctNo) {
+        return `${patient.firstName} ${patient.lastName} - ${patient.acctNo}`;
+      } else {
+        return 'Unknown Patient'; // Or handle this case as appropriate
+      }
+    },
   },
 };
 </script>
---
+++ pages/patient/[id]/reports/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'PatientReport',
+  components: {},
+  data() {
+    return {};
+  },
+  async mounted() {},
+  methods: {},
+};
+</script>
