36a07c927f24cd9e623a90a4218bb03fbf93ca62
Ben Webb <ben.webb340@gmail.com>
2024-01-12 18:20:33 +0900

    improve light theme
    made background color a very light grey so that it has some contrast with the white components on the screen. also made the sidebar always dark theme so that its dark even when light theme is enabled (to match the style deck).

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,23 +1,32 @@
-import { createVuetify } from 'vuetify';
+import { ThemeDefinition, createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
-// const myTheme: ThemeDefinition = {
-//   colors: {
-//     primary: '#EAEAEA',
-//     secondary: '#fff012',
-//   },
-// };
+const customLight: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#f0f6fc',
+    surface: '#FFFFFF',
+    primary: '#466afa',
+    'primary-darken-1': '#3700B3',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+};
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components,
     directives,
     theme: {
-      // themes: {
-
-      // }
       defaultTheme: 'dark',
+      themes: {
+        customLight,
+      },
     },
   });
 
