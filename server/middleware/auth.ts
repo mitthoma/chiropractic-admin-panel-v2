f1e3c74b48d283c1456dd771c224236b59140ca5
Ben Webb <ben.webb340@gmail.com>
2024-02-26 17:45:54 +0900

    auth mostly working, just need firebase credentials

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,13 +1,44 @@
-export default defineEventHandler((event) => {
-  if (event.path.includes('patient/')) {
+import { getAuth } from 'firebase-admin/auth';
+
+export default defineEventHandler(async (event) => {
+  if (event.path.includes('/patient/delete-patient')) {
     if (getMethod(event) !== 'OPTIONS') {
       const authHeader = event.node.req.headers.authorization;
-      console.log(
-        'auth middleware!',
-        `method: ${getMethod(event)}`,
-        `path: ${event.path}`,
-        `authHeader: ${authHeader}`
-      );
+      if (!authHeader) {
+        console.log('authentication failed: no auth header present');
+        console.log(
+          'auth middleware: failure...',
+          `method: ${getMethod(event)}`,
+          `path: ${event.path}`,
+          `authHeader: ${authHeader}`
+        );
+        event.node.res.statusCode = 401;
+        return 'UNAUTHORIZED';
+      }
+      try {
+        const auth = getAuth();
+        const decodedToken = await auth.verifyIdToken(authHeader);
+        console.log(
+          'auth middleware: success!',
+          `method: ${getMethod(event)}`,
+          `path: ${event.path}`,
+          `authHeader: ${authHeader}`,
+          `uid: ${decodedToken.uid}`,
+          `user-email: ${decodedToken.email}`
+        );
+      } catch {
+        console.log(
+          'authentication failed: error occurred while verifying token'
+        );
+        console.log(
+          'auth middleware: failure...',
+          `method: ${getMethod(event)}`,
+          `path: ${event.path}`,
+          `authHeader: ${authHeader}`
+        );
+        event.node.res.statusCode = 401;
+        return 'UNAUTHORIZED';
+      }
     }
   }
 });
